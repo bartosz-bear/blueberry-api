336d09be205cc3d48aec813f725867be373aaf80
lazarus_zamenhof <lazarus.zamenhof@gmail.com>
2015-06-09 21:20:51 +0200

    Included all C# files.

---
+++ temp/SynchronizeTaskPaneAndRibbon/SynchronizeTaskPaneAndRibbon/ManageTaskPaneRibbon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace SynchronizeTaskPaneAndRibbon
+{
+    public partial class ManageTaskPaneRibbon
+    {
+        private void ManageTaskPaneRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void toggleButton1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Globals.ThisAddIn.TaskPane.Visible = ((RibbonToggleButton)sender).Checked;
+
+        }
+    }
+}
---
+++ temp/SynchronizeTaskPaneAndRibbon/SynchronizeTaskPaneAndRibbon/ThisAddIn.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace SynchronizeTaskPaneAndRibbon
+{
+    public partial class ThisAddIn
+    {
+        private TaskPaneControl taskPaneControl1;
+        private Microsoft.Office.Tools.CustomTaskPane taskPaneValue;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            taskPaneControl1 = new TaskPaneControl();
+            taskPaneValue = this.CustomTaskPanes.Add(
+                taskPaneControl1, "MyCustomTaskPane");
+            taskPaneValue.VisibleChanged +=
+                new EventHandler(taskPaneValue_VisibleChanged);
+        }
+
+        private void taskPaneValue_VisibleChanged(object sender, System.EventArgs e)
+        {
+            Globals.Ribbons.ManageTaskPaneRibbon.toggleButton1.Checked =
+                taskPaneValue.Visible;
+        }
+
+        public Microsoft.Office.Tools.CustomTaskPane TaskPane
+        {
+            get
+            {
+                return taskPaneValue;
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
